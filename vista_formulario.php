03152bf3a6d2c8865c727b370f90d50d976ecead
brivelaf <facubrivela@gmail.com>
2025-05-27 16:16:30 -0300

    das

--- vista_formulario.php
+++ vista_formulario.php
@@ -3,7 +3,7 @@ header("Content-type: application/json");
 include("conexion.php");
 
 $tabla = [];
-$sql = 'SELECT * FROM formulario_principal';
+$sql = 'SELECT * FROM datos';
 $res = $conexion->query($sql);
 while ($file = $res->fetch_assoc()) {
     $tabla[] = $file;
